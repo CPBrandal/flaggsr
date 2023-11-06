4a90bcb1ffbad34fd12dbf96c6af1663ff54e9d2
PhilantropistB <120064430+PhilantropistB@users.noreply.github.com>
2023-11-06 01:03:54 +0100

    fasterImage.js

--- resetIc.js
+++ resetIc.js
@@ -1,64 +1,48 @@
-function resetIc(){
-    if(besoekt.size == 0){
+function resetIc() {
+    if (besoekt.size === 0) {
         showCongratulationsMessage();
         document.getElementById("imageContainer").style.display = "none";
         document.getElementById("titleLabel").style.display = "none";
         document.getElementById("score").style.display = "none";
         return;
     }
-    let randomFlag = "";
-    let randomCountry = "";
+
     let randomPair = getRandom();
-    randomCountry = randomPair.country;
-    randomFlag = randomPair.flag;
+    let randomCountry = randomPair.country;
+    let randomFlag = randomPair.flag;
     besoekt.delete(randomCountry);
 
     land = randomCountry;
-    
-    // When input matches the expected value, replace the image
-    let imageContainer = document.getElementById("imageContainer");
-    let newImage = document.createElement("img");
-    newImage.id = "newImage";
-    newImage.src = randomFlag; 
-    newImage.alt = "Another Image";
 
-    newInput = document.createElement("input");
-    newInput.type = "text";
+    // Get the existing image element
+    let imageElement = document.getElementById("mainImage");
+
+    // Update the existing image element's attributes
+    imageElement.src = randomFlag;
+    imageElement.alt = "Another Image";
+
+    // Reuse the existing input element and just update its attributes
+    newInput = document.getElementById("inputBox");
     newInput.style.display = "none";
     newInput.placeholder = "Which country";
-
-    // Clear the container and add new elements
-    imageContainer.innerHTML = "";
-    imageContainer.appendChild(newImage);
-    imageContainer.appendChild(newInput);
 
     const tall = Math.floor(Math.random() * 4) + 1;
     let liste = new Array();
 
     for (let i = 1; i <= 4; i++) {
-    const newButton = document.createElement("button");
-    newButton.id = "btn" + i;
-    if(i === tall){
-        newButton.textContent = land; 
-    } else {
-        newButton.textContent = getRandomFullListe().country;
-        while(liste.includes(newButton.textContent) || newButton.textContent === land){
+        let newButton = document.getElementById("btn" + i);
+        newButton.style.backgroundColor = '';  // Reset button background color
+        newButton.style.border = '';  // Reset button border
+
+        if (i === tall) {
+            newButton.textContent = land;
+        } else {
             newButton.textContent = getRandomFullListe().country;
+            while (liste.includes(newButton.textContent) || newButton.textContent === land) {
+                newButton.textContent = getRandomFullListe().country;
+            }
         }
-        liste.push(newButton.textContent);
-    }
-    // Add a click event listener to the new button
-    newButton.addEventListener("click", knapp);
-
-    // Append the new button to the imageContainer
-    imageContainer.appendChild(newButton);
     }
 
-    newInput.addEventListener("focus", () => {
-    newInput.style.opacity = 0;
-    setTimeout(() => newInput.style.opacity = 1);
-    });
-
-    newInput.addEventListener("keydown", handleInput);
     updateScore(true);
 }
